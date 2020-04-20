1fb0c288ae493052ed86d6be2db331535bd68381
tikkyjoker <tikkyjoker@gmail.com>
2020-04-20 15:28:06 +0700

    create bundle js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+var _ = require('lodash');
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
+console.log(_)
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
